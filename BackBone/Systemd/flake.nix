f65f829e51f5081d0db89fa62f331ee69ae1515a
casey <caseyriley94@gmail.com>
2026-04-27 05:33:58 -0400

    push-pull

--- BackBone/Systemd/flake.nix
+++ BackBone/Systemd/flake.nix
@@ -4,20 +4,26 @@
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systemd_source.url = "github:systemd/systemd";
+    systemd_liberated.url = "github:systemd/systemd";
+
   };
 
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
-        ./systemd.nix
+        ./systemd/flake-module.nix
       ];
-      systems = [ "x86_64-linux" "aarch64-darwin" ];
-      perSystem = { config, self', inputs', ... }: {
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
         # Per-system attributes can be defined here. The self' and inputs'
         # module parameters provide easy access to attributes of the same
         # system.
 
-        packages.figlet = inputs'.nixpkgs.legacyPackages.figlet;
+        packages.default = config.packages.systemd;
+
+        packages.systemd = pkgs.callPackage ./systemd/package.nix { };
+        checks.systemd = pkgs.callPackage ./systemd/test.nix {
       };
       flake = {
         # The usual flake attributes can be defined here, including system-
